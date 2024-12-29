59f509a8913f5b4b28ebd8924caa4e60d0dbd447
Adservx <imserv67@gmail.com>
2024-12-29 14:11:24 +0545

    ok

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World!');
-});
-
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
-module.exports = server;
+module.exports = (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.status(200).send('Hello World!');
+};
